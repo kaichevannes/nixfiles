5e45cf49787a27356207f9b8415927dce1e87438
Kai Chevannes <chevannes.kai@gmail.com>
2025-10-08 17:14:14 +0100

    feat: programs enabled and fish default shell:w

---
+++ home.nix
@@ -0,0 +1,59 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "focus";
+  home.homeDirectory = "/home/focus";
+
+  home.packages = [
+  ];
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "25.05";
+
+  programs.home-manager.enable = true;
+  programs.helix.enable = true;
+  programs.lazygit.enable = true;
+  programs.tmux.enable = true;
+  programs.yazi.enable = true;
+
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting
+    '';
+  };
+
+  # Launch fish from bash
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Kai Chevannes";
+    userEmail = "chevannes.kai@gmail.com";
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    enableTransience = true;
+    settings = {
+      add_newline = false;
+    };
+  };
+}
